0c1668485392bdec5367e701fb25525c6f6dd108
gerardodrm <gerardo.delarosama@udlap.mx>
2017-11-05 22:55:15 -0600

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import * as BooksAPI from './BooksAPI'
-import BookPreview from './BookPreview'
+import { Route } from 'react-router-dom'
+import Library from './Library'
+import SearchView from './SearchView'
 
 class BooksApp extends React.Component {
 
-  construct() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSearchPage: false,
+      currentlyReadingBooks: [],
+      wantToReadBooks: [],
+      readBooks: [],
+      books: []
+    };
   }
 
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-    currentlyReadingBooks: [],
-    wantToReadBooks: [],
-    readBooks: [],
-    books: []
-  }
 
   componentDidMount() {
     // Using BookAPI getAll to get all books from server
@@ -52,77 +49,18 @@ class BooksApp extends React.Component {
     })
   }
 
-
-
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                     {this.state.currentlyReadingBooks.map((book) =>
-                       <BookPreview key={book.id} book={book} updateBook={(b,s) => this.updateBookShelf(b, s)}/>
-                     )}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {this.state.wantToReadBooks.map((book) =>
-                      <BookPreview key={book.id} book={book} updateBook={(b,s) => this.updateBookShelf(b, s)}/>
-                    )}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {this.state.readBooks.map((book) =>
-                      <BookPreview key={book.id} book={book} updateBook={(b,s) => this.updateBookShelf(b, s)}/>
-                    )}
-                    </ol>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+        <Route exact path='/' render={() => (
+          <Library
+            {...this.state}
+            updateBookShelf={(b,s) => this.updateBookShelf(b,s)}
+          />
+        )}/>
+        <Route path='/search' render={({ history }) => (
+          <SearchView/>
+        )}/>
       </div>
     )
   }
---
+++ src/Library.js
@@ -0,0 +1,57 @@
+import React, {Component, PropTypes} from 'react';
+import BookPreview from './BookPreview'
+import { Link } from 'react-router-dom'
+
+class Library extends Component {
+
+
+  render() {
+    const {currentlyReadingBooks, wantToReadBooks, readBooks, updateBookShelf} = this.props
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                 {currentlyReadingBooks.map((book) =>
+                   <BookPreview key={book.id} book={book} updateBook={updateBookShelf}/>
+                 )}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                {wantToReadBooks.map((book) =>
+                  <BookPreview key={book.id} book={book} updateBook={updateBookShelf}/>
+                )}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                {readBooks.map((book) =>
+                  <BookPreview key={book.id} book={book} updateBook={updateBookShelf}/>
+                )}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to='/search'>Add a book</Link>
+        </div>
+      </div>
+
+    );
+  }
+}
+module.exports = Library;
